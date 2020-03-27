7712e3d6e439fc9a61701c6e9805ecbc16a411aa
Ananda Reis <ananda.queiroz70@hotmail.com>
2020-03-27 12:25:59 -0300

    back-end do app

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,28 @@
+const request = require('supertest')
+const app = require('../../src/app')
+const connection = require('../../src/database/connection')
+
+describe('ONG', ()=> {
+    beforeEach( async ()=>{
+        await connection.migrate.rollback()
+        await connection.migrate.latest()
+    })
+
+    afterAll(async()=>{
+        await connection.destroy()
+    })
+
+    it('shoud be able to create a new ONG', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            .send({
+                name: "APAD",
+                email: "contato@apad.com.br",
+                whatsapp: "5767675774",
+                city: "Salvador",
+                uf: "ba"
+            });
+        expect(response.body).toHaveProperty('id')
+        expect(response.body.id).toHaveLength(8)
+    })
+})  
